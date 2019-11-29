c3957e8e2567947ab0a9b42af460bbbccf4f9522
Christopher Hayashi <Chrisrhayashi@gmail.com>
2019-11-28 21:38:53 -0800

    spotify-this-song command implemented correctly(searchSong function)

--- liri.js
+++ liri.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 //import the axios package
 var axios = require("axios");
 
+//import the fs package
+var fs = require("file-system");
+
 //import the moment package
 var moment = require("moment");
 
@@ -21,40 +24,81 @@ var command = process.argv[2];
 
 console.log("");
 
+function searchConcert(artist) {
+
+    //perform a get request on axios using bandsintown
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(response) {
+
+            //then console log the venue
+            console.log("Venue: " + response.data[0].venue.name);
+            
+            //console log the location
+            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+            
+            //retrieve the datetime of the event
+            var date = (response.data[0].datetime).split("T");
+            
+            //retrieve only the date of the event
+            date = date[0];
+
+            //convert the format of the date using moment.js
+            date = moment(date).format("MM/DD/YYYY");
+
+            //console log the formatted date
+            console.log("Date: " + date);
+        })
+        .catch(function(error) {
+            console.log("There are no upcoming concerts for that artist.");
+        });
+}
+
 function searchSong(song) {
-    spotify.search({
-        type: "track",
-        query: song,
-        limit: 1
-    }, function(error, response) {
 
-        if (error) {
-            searchSong("The Sign Ace of Base");
+    if (song === undefined) {
+        searchSong("The Sign Ace of Base");
+    }
 
-        } else {
-            console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
-            console.log("Song: " + response.tracks.items[0].name);
-            console.log("Preview Link: " + response.tracks.items[0].external_urls.spotify);
-            console.log("Album: " + response.tracks.items[0].album.name);
-        }
-    });
+    else {
+        spotify.search({
+            type: "track",
+            query: song
+            // limit: 1
+        }, function(error, response) {
+
+            if (error) {
+                console.log("That song does not exist on spotify.");
+
+            } else {
+                console.log("Artist: " + response.tracks.items[0].album.artists[0].name);
+                console.log("Song: " + response.tracks.items[0].name);
+                console.log("Preview Link: " + response.tracks.items[0].external_urls.spotify);
+                console.log("Album: " + response.tracks.items[0].album.name);
+            }
+        });
+}
 }
 
 function searchMovie(movie) {
 
-    var name = movie.split(" ").join("+");
+    if (movie === undefined)
+        searchMovie("Mr. Nobody");
+    else {
 
-    axios.get("http://www.omdbapi.com/?t=" + name + "&apikey=9f733008")
-    .then(function(response) {
-        console.log("Movie: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-        console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-        console.log("Produced in: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    });
+        var name = movie.split(" ").join("+");
+        
+        axios.get("http://www.omdbapi.com/?t=" + name + "&apikey=9f733008")
+        .then(function(response) {
+            console.log("Movie: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Produced in: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        });
+    }
 }
 
 //if the command is equal to "concert-this"
@@ -63,28 +107,7 @@ if (command === "concert-this") {
     //Store the artist entered by the user
     var artist = process.argv[3];
 
-    //perform a get request on axios using bandsintown
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-    .then(function(response) {
-
-        //then console log the venue
-        console.log("Venue: " + response.data[0].venue.name);
-        
-        //console log the location
-        console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
-        
-        //retrieve the datetime of the event
-        var date = (response.data[0].datetime).split("T");
-        
-        //retrieve only the date of the event
-        date = date[0];
-
-        //convert the format of the date using moment.js
-        date = moment(date).format("MM/DD/YYYY");
-
-        //console log the formatted date
-        console.log("Date: " + date);
-    });
+    searchConcert(artist);
 }
 
 else if (command === "spotify-this-song") {
@@ -100,13 +123,26 @@ else if (command === "movie-this") {
     //store the name of the movie
     var movie = process.argv[3];
 
-    if (movie === undefined) {
-        searchMovie("Mr. Nobody");
-    } else {
-        searchMovie(movie);
-    }
+    searchMovie(movie);
 }
 
 else if (command === "do-what-it-says") {
-    
+
+    fs.readFile("random.txt", "utf8", function(error, contents) {
+
+        if(error) {
+            return console.log("Error occurred: " + error);
+        }
+
+        var text = contents.split(",");
+        var textCommand = text[0];
+        var textName = text[1];
+
+        if (textCommand === "concert-this")
+            searchConcert(textName);
+        else if (textCommand === "spotify-this-song")
+            searchSong(textName);
+        else if (textCommand === "movie-this")
+            searchMovie(textName);
+    });
 }
